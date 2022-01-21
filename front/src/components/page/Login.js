73211c2204e555da6429d68d62cc8237571fec47
ghghwldk <ghghwldk@gmail.com>
2022-01-21 21:35:06 +0900

    login 화면 추가

---
+++ front/src/components/page/Login.js
@@ -0,0 +1,88 @@
+import React, {useState, useEffect, useRef, useMemo} from 'react';
+import axios from 'axios'
+import "./Login.css";
+import { Link, Route, Switch, useLocation} from 'react-router-dom';
+import { useHistory } from 'react-router-dom/cjs/react-router-dom.min';
+import {useSelector, connect, useDispatch} from 'react-redux'
+
+function useQuery() {
+  const history = useHistory()
+  const { search } = useLocation();
+
+  return React.useMemo(() => new URLSearchParams(search), [search]);
+}
+
+const Login = ()=>{
+  let state = useSelector((state) => state )
+  const idRef = useRef();
+  const passwordRef = useRef();
+  let query= useQuery();
+  const history = useHistory()
+  const dispatch = useDispatch()
+
+  const processLogin = ()=> {
+    const id = idRef.current.value
+    const password = passwordRef.current.value
+    
+    const parameter = {
+      'id': id,
+      'password': password, 
+    }
+    axios({
+      url: `/user/login`,
+      method: 'POST',
+      data:JSON.stringify(parameter),
+      headers: { 'content-type': 'application/json' },
+      async: true
+    }).then((res) => {
+      if(res.data.message==='success'){
+        const redirectUrl = query.get("redirectUrl")
+        const role = res.data.role
+        // set redux.
+        dispatch({type:'register', payload : {role : role} }) 
+
+        if(redirectUrl=== null){
+          history.push(`/`)
+        } else {
+          history.push(redirectUrl)
+        }
+      }
+      
+    })
+  }
+
+  useEffect(
+    () => {
+    }, []
+  )
+
+  return (
+      <div className="Login">
+        <div>{state.role}</div>
+        <p>ID</p>
+        <input id="id" name="id" type="text"
+          ref= {idRef}
+        ></input>
+        <p>PW</p>
+        <input id="password" name="password" type="password"
+          ref= {passwordRef}
+        ></input>
+        <button id="submit"
+          onClick={()=>{
+            processLogin()
+          }}
+        >
+          login
+        </button>
+      </div>
+    )
+}
+
+export default Login
+// function state를props화(state){
+//   return {
+//     state : state
+//   }
+// }
+
+// export default connect(state를props화)(Login);
