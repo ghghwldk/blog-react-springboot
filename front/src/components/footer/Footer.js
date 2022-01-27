1cdef5c8b4498943b60edad047d28f9b6af77479
ghghwldk <ghghwldk@gmail.com>
2022-01-28 00:07:55 +0900

    Footer 디자인 변경

--- front/src/components/footer/Footer.js
+++ front/src/components/footer/Footer.js
@@ -8,22 +8,7 @@ const Footer = ()=>{
 
   return (
     <div className='Footer'>
-        <img 
-          src={growth}
-          width='80'
-          alt='growth'  
-        />
-        <p>성장하는 개발자가 되자!</p>
-        <p>ghghwldk@gmail.com</p>
-        
-        <div className="history">
-          <FontAwesomeIcon 
-            icon={faHistory}
-            className="history-icon" 
-            // onClick={save}
-          />
-        </div>
-        
+        <font>ghghwldk@gmail.com2</font>
     </div>
   )
 }
