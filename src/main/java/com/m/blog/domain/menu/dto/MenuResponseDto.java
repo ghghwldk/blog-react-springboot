0df5eaa138bc7fa8a15fd87f95079c986bd115bf
ghghwldk <ghghwldk@gmail.com>
2024-02-15 17:19:45 +0900

    service 코드 간편해지게 변경

--- src/main/java/com/m/blog/domain/menu/dto/MenuResponseDto.java
+++ src/main/java/com/m/blog/domain/menu/dto/MenuResponseDto.java
@@ -1,11 +1,14 @@
 package com.m.blog.domain.menu.dto;
 
 import com.m.blog.domain.boardCollection.dto.BoardAggregationDto;
+import com.m.blog.domain.boardCollection.entity.BoardCollection;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 @Data
 @Builder
@@ -23,4 +26,26 @@ public class MenuResponseDto {
         List<BoardAggregationDto> boardInformationInMenuDtos;
     }
 
+    public static MenuResponseDto of(List<BoardCollection> boardCollections,
+                                List<BoardAggregationDto> boardAggregationDtos){
+        Map<Integer, List<BoardAggregationDto>> dtoPerBoardCollections = boardAggregationDtos.stream()
+                .collect(Collectors.groupingBy(BoardAggregationDto::getBoardCollectionId,
+                        Collectors.toList()));
+
+        List<MenuResponseDto.Nested> nesteds = boardCollections.stream()
+                .map(bc -> {
+                    List<BoardAggregationDto> filtered = dtoPerBoardCollections.get(bc.getId());
+                    return MenuResponseDto.Nested.builder()
+                            .boardCollectionName(bc.getName())
+                            .boardCollectionId(bc.getId())
+                            .postingCount(filtered != null ? filtered.size() : 0)
+                            .boardInformationInMenuDtos(filtered)
+                            .build();
+                })
+                .collect(Collectors.toList());
+
+        return MenuResponseDto.builder()
+                .nesteds(nesteds)
+                .build();
+    }
 }
--- src/main/java/com/m/blog/domain/menu/service/MenuServiceImpl.java
+++ src/main/java/com/m/blog/domain/menu/service/MenuServiceImpl.java
@@ -1,15 +1,10 @@
 package com.m.blog.domain.menu.service;
 
-import com.m.blog.domain.boardCollection.dto.BoardAggregationDto;
-import com.m.blog.domain.boardCollection.entity.BoardCollection;
 import com.m.blog.domain.boardCollection.service.BoardCollectionService;
 import com.m.blog.domain.menu.dto.MenuResponseDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.LinkedList;
-import java.util.List;
 
 @Service
 @Transactional(readOnly = true)
@@ -17,39 +12,11 @@ import java.util.List;
 public class MenuServiceImpl implements MenuService{
     private final BoardCollectionService boardCollectionService;
 
-
-    private MenuResponseDto get(List<BoardCollection> boardCollections,
-                            List<BoardAggregationDto> boardAggregationDtos){
-        List<MenuResponseDto.Nested> nesteds = new LinkedList<>();
-
-        for(BoardCollection bc: boardCollections){
-            int postingCount = 0;
-            String boardCollectionName= bc.getName();
-            int boardCollectionId= bc.getId();
-            List<BoardAggregationDto> InSpecificBoardCollection = new LinkedList<>();
-            for(BoardAggregationDto b: boardAggregationDtos){
-                if(b.getBoardCollectionId() == boardCollectionId){
-                    InSpecificBoardCollection.add(b);
-                    postingCount += b.getPostingCount();
-                }
-            }
-            nesteds.add(MenuResponseDto.Nested.builder()
-                    .boardCollectionName(boardCollectionName)
-                    .boardCollectionId(boardCollectionId)
-                    .postingCount(postingCount)
-                    .boardInformationInMenuDtos(InSpecificBoardCollection)
-                    .build());
-        }
-
-        return MenuResponseDto.builder()
-                .nesteds(nesteds)
-                .build();
-    }
-
     @Override
     public MenuResponseDto get(){
-        return get(boardCollectionService.getBoardCollections(),
-                boardCollectionService.getBoardAggregationDtos());
+        return MenuResponseDto.of(
+                boardCollectionService.getBoardCollections(),
+                boardCollectionService.getBoardAggregationDtos()
+        );
     }
-
 }
