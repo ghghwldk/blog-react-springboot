f8f470090284e1c836302000a05d99e2ce7f6872
ghghwldk <ghghwldk@gmail.com>
2024-03-11 19:32:24 +0900

    kotlin 제거

--- src/main/java/com/m/blog/domain/menu/service/MenuService.java
+++ src/main/java/com/m/blog/domain/menu/service/MenuService.java
@@ -1,23 +1,23 @@
-//package com.m.blog.domain.menu.service;
-//
-//import com.m.blog.domain.boardCollection.application.port.persistence.GetBoardCollectionPort;
-//import com.m.blog.domain.menu.infrastructure.web.dto.MenuResponse;
-//import com.m.blog.domain.menu.application.usecase.MenuUsecase;
-//import lombok.RequiredArgsConstructor;
-//import org.springframework.stereotype.Service;
-//import org.springframework.transaction.annotation.Transactional;
-//
-//@Service
-//@Transactional(readOnly = true)
-//@RequiredArgsConstructor
-//public class MenuService implements MenuUsecase {
-//    private final GetBoardCollectionPort getBoardCollectionQuery;
-//
-//    @Override
-//    public MenuResponse get(){
-//        return MenuResponse.of(
-//                getBoardCollectionQuery.getBoardCollectionEntities(),
-//                getBoardCollectionQuery.getBoardAggregationDtos()
-//        );
-//    }
-//}
+package com.m.blog.domain.menu.service;
+
+import com.m.blog.domain.boardCollection.application.port.persistence.GetBoardCollectionPort;
+import com.m.blog.domain.menu.infrastructure.web.dto.MenuResponse;
+import com.m.blog.domain.menu.application.usecase.MenuUsecase;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MenuService implements MenuUsecase {
+    private final GetBoardCollectionPort getBoardCollectionQuery;
+
+    @Override
+    public MenuResponse get(){
+        return MenuResponse.of(
+                getBoardCollectionQuery.getBoardCollectionEntities(),
+                getBoardCollectionQuery.getBoardAggregationDtos()
+        );
+    }
+}
